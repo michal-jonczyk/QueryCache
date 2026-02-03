0b7f96ddbf0e0cb831001990d80e3a5b3ed33379
Michał Jończyk <michaljonczyk151@gmail.com>
2026-02-03 15:06:33 +0100

    Add Vercel URL to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173",
+        "https://query-cache.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
